1ba59e75ad1e6954f085657103204d33178ecd7e
rukiamuq-hard <kusagaid@gmail.com>
2026-09-02 17:19:40 +0300

    Bug fix

--- telegram.go
+++ telegram.go
@@ -5,19 +5,15 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"time"
 )
 
-func (c *Client) Telegram(req TelegramRequest) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
+func (c *Client) Telegram(ctx context.Context, req TelegramRequest) error {
 	data, err := json.Marshal(req)
 	if err != nil {
 		return err
 	}
 
-	resp, err := http.NewRequestWithContext(ctx, "POST", c.baseURL+"/api/tg", bytes.NewReader(data))
+	resp, err := http.NewRequestWithContext(ctx, http.MethodPost, c.baseURL+"/api/tg", bytes.NewReader(data))
 	if err != nil {
 		return err
 	}
