04bf37b6227787c4539aec17b80219d973869010
rukiamuq-hard <kusagaid@gmail.com>
2026-09-09 17:48:36 +0300

    discord added

---
+++ discord.go
@@ -0,0 +1,34 @@
+package ntfy
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func (c *Client) Discord(ctx context.Context, dr DiscordRequest) error {
+	data, err := json.Marshal(dr)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.NewRequestWithContext(ctx, http.MethodPost, c.baseURL+"/api/ds", bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	resp.Header.Add("Content-type", "application/json")
+
+	res, err := c.client.Do(resp)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("server returned status %d", res.StatusCode)
+	}
+
+	return nil
+}
--- models.go
+++ models.go
@@ -5,3 +5,8 @@ type TelegramRequest struct {
 	ChatID  int64  `json:"chat_id"`
 	Message string `json:"message"`
 }
+
+type DiscordRequest struct {
+	WHook   string `json:"webhook"`
+	Message string `json:"message"`
+}
